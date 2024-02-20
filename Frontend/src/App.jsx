7e4cfca1f3f1f17e835787c8ddd31315f0e13613
Tiirnako <treeleaves229@gmail.com>
2024-02-19 19:36:07 -0600

    Backend initial Commit

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -5,13 +5,16 @@ function App() {
 
   return (
     <>
-      <Map mapboxAccessToken="pk.eyJ1IjoidGlpcm5ha28iLCJhIjoiY2xzb2JiZXI4MGRiODJrb3c5NnlmZnRjYyJ9.Fv2ex2k4_1efbXdhZjMl1Q"
-      initialViewState={{
-        longitude: -95.992775,
-        latitude:  36.153980,
-        zoom: 12}}
-        style={{width: 500, height: 500}}
-        mapStyle={"mapbox://styles/tiirnako/clsoeendf04ev01nlbuki52pf"}/>
+      <div className="w-full h-full">
+        <Map mapboxAccessToken="pk.eyJ1IjoidGlpcm5ha28iLCJhIjoiY2xzb2JiZXI4MGRiODJrb3c5NnlmZnRjYyJ9.Fv2ex2k4_1efbXdhZjMl1Q"
+          style={{height: "100vh", overflow: 'hidden'}}
+        initialViewState={{
+          longitude: -95.992775,
+          latitude:  36.153980,
+          zoom: 12}}
+          mapStyle={"mapbox://styles/tiirnako/clsoeendf04ev01nlbuki52pf"}>
+          </Map>
+      </div>
     </>
   )
 }
