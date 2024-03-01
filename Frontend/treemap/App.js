5819de2577a8fcd3ee2640578b87ca7388d82d22
Tiirnako <treeleaves229@gmail.com>
2024-03-01 11:45:59 -0600

    Add default location + Tailwind

--- Frontend/treemap/App.js
+++ Frontend/treemap/App.js
@@ -1,33 +1,46 @@
-import React from 'react';
-import { StyleSheet, View, Text } from 'react-native';
-import Mapbox from '@rnmapbox/maps';
+import React from "react";
+import { StyleSheet, View, Text } from "react-native";
+import Mapbox from "@rnmapbox/maps";
 
-Mapbox.setAccessToken('pk.eyJ1IjoidGlpcm5ha28iLCJhIjoiY2xzb2JiZXI4MGRiODJrb3c5NnlmZnRjYyJ9.Fv2ex2k4_1efbXdhZjMl1Q');
+Mapbox.setAccessToken(
+  "pk.eyJ1IjoidGlpcm5ha28iLCJhIjoiY2xzb2JiZXI4MGRiODJrb3c5NnlmZnRjYyJ9.Fv2ex2k4_1efbXdhZjMl1Q"
+);
 
 const App = () => {
   return (
     <View style={styles.page}>
       <View style={styles.container}>
-        <Text>Hello Maps</Text>
-        <Mapbox.MapView style={styles.map} />
+        <Mapbox.MapView
+          scaleBarEnabled={false}
+          styleURL="mapbox://styles/tiirnako/clsoeendf04ev01nlbuki52pf"
+          style={styles.map}
+        >
+          <Mapbox.Camera
+            zoomLevel={10}
+            centerCoordinate={[-95.92682, 36.135769]}
+          />
+        </Mapbox.MapView>
+        <View className="bg-black">
+          <Text className="text-white">Tailwind is working</Text>
+        </View>
       </View>
     </View>
   );
-}
+};
 
 export default App;
 
 const styles = StyleSheet.create({
   page: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+    justifyContent: "center",
+    alignItems: "center",
   },
   container: {
-    height: 300,
-    width: 300,
+    height: "100%",
+    width: "100%",
   },
   map: {
-    flex: 1
-  }
+    flex: 1,
+  },
 });
