65d54de0c699ea7e6de5b5525e7441573cc8f441
Tiirnako <treeleaves229@gmail.com>
2024-03-28 13:38:44 -0500

    Refactor SelectedSite component to improve UI and functionality

--- components/Site/Selected.js
+++ components/Site/Selected.js
@@ -15,8 +15,41 @@ const SelectedSite = () => {
 
   return (
     <>
-      <View>
-        <ScrollView>
+      <View className="w-full flex">
+        
+        <View className="w-full flex flex-row items-center justify-between pb-4">
+          <Text className="font-bold text-lg text-white">Site: {sliderTitle}</Text>
+          <View className="flex flex-row gap-2">
+            <TouchableHighlight
+              className="flex flex-row items-center justify-center text-center bg-[#4e545f56]  rounded-lg"
+              onPress={() => {
+                setCurrentScreen("AddTree");
+                setShowCustomTree(false);
+                sliderRef.current.show();
+              }}
+              underlayColor={"transparent"}
+            >
+              <View className="flex flex-row justify-center items-center rounded-lg">
+                <Text className="text-white ml-3">Add Tree</Text>
+                <Icons name="add" size={40} color="#374151"></Icons>
+              </View>
+            </TouchableHighlight>
+            <TouchableHighlight
+              onPress={() => {
+                setCurrentScreen("siteList");
+                setShowCustomTree(false);
+                setSelectedTrees(null);
+                sliderRef.current.show();
+              }}
+              underlayColor={"transparent"}
+              className="bg-[#4e545f56] rounded-full"
+            >
+              <Icons name="close" size={40} color="#374151"></Icons>
+            </TouchableHighlight>
+          </View>
+        </View>            
+
+        <ScrollView className="h-[75%] rounded-lg">
           {selectedTrees && selectedTrees.map((tree, index) => (
             <TouchableHighlight
               key={index}
@@ -28,14 +61,14 @@ const SelectedSite = () => {
                   toValue: 200,
                 });
               }}
+              activeOpacity={0.6}
+              underlayColor="#d4dbe044"
+              className="flex flex-row justify-between p-4 mb-3 w-full bg-[#d4dbe044] border-b border-gray-700 rounded-xl"
             >
-              <View>
-                <Text className="font-bold text-lg text-white">{tree.properties.treeID.toString().padStart(4, "0")}</Text>
+              <View className="flex flex-row w-full justify-between">
+                <Text className="font-bold text-lg text-white">Tree: {tree.properties.treeID.toString().padStart(4, "0")}</Text>
                 <Text className="font-bold text-lg text-white">{tree.properties.treeSpecies}</Text>
-                <Text className="font-bold text-lg text-white">{tree.properties.treeFamily}</Text>
-                <Text className="font-bold text-lg text-white">{tree.properties.dateCreated}</Text>
                 {tree.properties.needsWork && <Text className="font-bold text-lg text-red">Needs Work</Text>}
-
               </View>
             </TouchableHighlight>
           ))}
