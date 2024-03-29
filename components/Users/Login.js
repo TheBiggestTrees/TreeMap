fd599d6e10a5afedc4972c623043ff713d6ee2a1
Tiirnako <treeleaves229@gmail.com>
2024-03-28 22:26:32 -0500

    Refactor Login component and add login functionality

--- components/Users/Login.js
+++ components/Users/Login.js
@@ -1,8 +1,15 @@
-import React, { Component } from "react";
-import { StatusBar, Text, TextInput, View } from "react-native";
+import React, { useState } from "react";
+import { StatusBar, Text, TextInput, Touchable, TouchableHighlight, View } from "react-native";
+import { useAuth } from "../../context/AuthContext";
 
-export class Login extends Component {
-  render() {
+const Login = () => {
+
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [username, setUsername] = useState("");
+
+    const { onLogin } = useAuth(); 
+
     return (
       <View className="flex w-full h-full bg-gray-500 ">
         <StatusBar backgroundColor={"#6b7280"} />
@@ -11,11 +18,21 @@ export class Login extends Component {
           <View className="flex w-[80%] mt-3">
               <TextInput className="bg-white my-2 rounded-lg h-10 p-2" placeholder="Email or Username" />
               <TextInput className="bg-white my-2 rounded-lg h-10 p-2" placeholder="Password"  />
+              <TouchableHighlight className="w-32"
+              onPress={() => {
+                console.log("Login")
+              }}
+              activeOpacity={0.3}
+            underlayColor="#6b7280"
+               >
+
+                <Text>Login</Text>
+            </TouchableHighlight>
           </View>
         </View>
       </View>
     );
   }
-}
+
 
 export default Login;
