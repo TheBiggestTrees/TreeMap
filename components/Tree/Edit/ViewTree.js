778a528fc01bddceacc5fc46156e8784bd1ca7cc
Tiirnako <treeleaves229@gmail.com>
2024-03-30 14:55:19 -0500

    Refactor ViewTree component layout and display

--- components/Tree/Edit/ViewTree.js
+++ components/Tree/Edit/ViewTree.js
@@ -11,40 +11,31 @@ const ViewTree = () => {
       <View className="flex flex-row items-center bg-slate-400 shadow-lg py-4 rounded-full">
         <View className="h-10 w-full flex flex-row justify-evenly">
             <View className="flex items-center justify-center">
-                <Text className="font-bold text-lg text-white">{workingTree.properties.treeID.toString().padStart(4, "0")}</Text>
-                <Text className="text-white font-bold text-lg">Tree</Text>
-            </View>
-            <View className="flex items-center justify-center">
                 <Text className="font-bold text-lg text-white">{sliderTitle.toString().padStart(4, "0")}</Text>
                 <Text className="text-white font-bold text-lg">Site</Text>
+            </View>
+            <View className="flex items-center justify-center">
+                <Text className="font-bold text-lg text-white">{workingTree.properties.treeID.toString().padStart(4, "0")}</Text>
+                <Text className="text-white font-bold text-lg">Tree</Text>
             </View>
         </View>
       </View>
 
-      <View className="flex items-center bg-slate-400 shadow-lg px-10 rounded-xl">
+      <View className="flex items-center bg-slate-400 shadow-lg px-10 py-4 rounded-xl">
         <Text className="text-white font-bold text-lg">
           Species: {workingTree.properties.treeSpecies}
-        </Text>
-        <Text className="text-white font-bold text-lg">
-          Family: {workingTree.properties.treeFamily}
         </Text>
         <Text className="text-white font-bold text-lg">
           Status: {workingTree.properties.status}
         </Text>
         <Text className="text-white font-bold text-lg">
+          DBH: {workingTree.properties.dbh}
+        </Text>
+        <Text className="text-white font-bold text-lg">
           Condition: {workingTree.properties.condition}
         </Text>
         <Text className="text-white font-bold text-lg">
-          Leaf Condition: {workingTree.properties.leafCondition}
-        </Text>
-        <Text className="text-white font-bold text-lg">
           Comment: {workingTree.properties.comment}
-        </Text>
-        <Text className="text-white font-bold text-lg">
-          Last Modified Date: {workingTree.properties.lastModifiedDate}
-        </Text>
-        <Text className="text-white font-bold text-lg">
-          Last Modified By: {workingTree.properties.lastModifiedBy}
         </Text>
         <Text className="text-white font-bold text-lg">
           Last Work Date: {workingTree.properties.lastWorkDate}
@@ -55,12 +46,9 @@ const ViewTree = () => {
         <Text className="text-white font-bold text-lg">
           Needs Work: {workingTree.properties.needsWork.toString()}
         </Text>
-        <Text className="text-white font-bold text-lg">
+        {workingTree.properties.needsWork && <Text className="text-white font-bold text-lg">
           Needs Work Comment: {workingTree.properties.needsWorkComment}
-        </Text>
-        <Text className="text-white font-bold text-lg">
-          DBH: {workingTree.properties.dbh}
-        </Text>
+        </Text>}
         <Text className="text-white font-bold text-lg">
           Date Created: {workingTree.properties.dateCreated}
         </Text>
@@ -74,7 +62,7 @@ const ViewTree = () => {
           Date Planted: {workingTree.properties.datePlanted}
         </Text>
         <Text className="text-white font-bold text-lg">
-          Photos: {workingTree.properties.photos}
+          Coordinates: [{workingTree.geometry.coordinates[0].toFixed(5)}, {workingTree.geometry.coordinates[1].toFixed(5)}]
         </Text>
       </View>
     </View>
