8c3d8b0b47f36d2e5ddda440d041f93010657e01
Tiirnako <treeleaves229@gmail.com>
2024-04-10 21:13:21 -0500

    Update InspectMain component to display working tree information

--- components/Tree/Edit/Inspect/InspectMain.js
+++ components/Tree/Edit/Inspect/InspectMain.js
@@ -1,11 +1,24 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Text, View } from "react-native";
+import ScreenContext from "../../../../context/screenContext";
 
 const InspectMain = () => {
+  const { workingTree } = useContext(ScreenContext);
+
   return (
-    <View>
-      <Text>InspectMain</Text>
-    </View>
+    <>
+      <View>
+        <Text className="text-white font-bold text-lg">
+          Inspecting Tree{" "}
+          {workingTree.properties.treeID.toString().padStart(4, "0")}
+        </Text>
+      </View>
+      <View className="bg-slate-400 flex w-full mt-4 rounded-lg grow p-4">
+        <View className="flex flex-row items-center justify-evenly">
+          <Text className="font-bold text-white text-lg">Hello world</Text>
+        </View>
+      </View>
+    </>
   );
 };
 
