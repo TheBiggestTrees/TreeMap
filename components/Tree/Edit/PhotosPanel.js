8f94ed223b3b757a2ec775a03f35d28ed9e26813
Tiirnako <treeleaves229@gmail.com>
2024-04-30 19:18:40 -0500

    Remove extra things from PhotosPanel.js

--- components/Tree/Edit/PhotosPanel.js
+++ components/Tree/Edit/PhotosPanel.js
@@ -20,29 +20,30 @@ const PhotosPanel = () => {
 
   useEffect(() => {
     const fetchImages = async () => {
-    if (images.length < 5) {
-      workingTree.properties.photos.map((photo) => {
-        if (photo !== "N/A") {
-          getImages(photo)
-            .then((res) => {
-              console.log(res);
-              if (!images) {
-                setImages([res]);
-              } else if (images.length < 5) {
-                if (!images.includes(res)) {
-                  setImages((prev) => [...prev, res]);
+      if (images.length < 5) {
+        workingTree.properties.photos.map((photo) => {
+          if (photo !== "N/A") {
+            getImages(photo)
+              .then((res) => {
+                console.log(res);
+                if (!images) {
+                  setImages([res]);
+                } else if (images.length < 5) {
+                  if (!images.includes(res)) {
+                    setImages((prev) => [...prev, res]);
+                  }
                 }
-              }
-            })
-            .catch((err) => {
-              console.error(err);
-              console.log("Error getting images, ", err.body.message);
-            });
-        }
-      });
-    }
+              })
+              .catch((err) => {
+                console.error(err);
+                console.log("Error getting images, ", err.body.message);
+              });
+          }
+        });
+      }
 
-    fetchImages();
+      fetchImages();
+    };
   }, []);
 
   return (
